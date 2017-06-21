8db1c6e7c0b45ab1a52c58ad3c6455a54d61153e
TingSyuanWang <dtvgood202@gmail.com>
2017-06-21 10:13:52 +0800

    campustour step4 view is working

--- resources/views/campusTour/step4.blade.php
+++ resources/views/campusTour/step4.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('title')
-    預約導覽 步驟三
+    預約導覽 步驟四
 @stop
 
 @section('content')
@@ -25,11 +25,11 @@
                                                 <div class="mt-step-number bg-white">2</div>
                                                 <div class="mt-step-title uppercase font-grey-cascade">選擇日期</div>
                                             </div>
-                                            <div class="col-md-3 mt-step-col active">
+                                            <div class="col-md-3 mt-step-col done">
                                                 <div class="mt-step-number bg-white">3</div>
                                                 <div class="mt-step-title uppercase font-grey-cascade">資料填寫</div>
                                             </div>
-                                            <div class="col-md-3 mt-step-col last">
+                                            <div class="col-md-3 mt-step-col last active">
                                                 <div class="mt-step-number bg-white">4</div>
                                                 <div class="mt-step-title uppercase font-grey-cascade">資料確認</div>
                                             </div>
@@ -42,7 +42,7 @@
                                                         <div class="portlet-title">
                                                             <div class="caption font-red-sunglo">
                                                                 <i class="icon-settings font-red-sunglo"></i>
-                                                                <span class="caption-subject bold uppercase">請完成以下資料的填寫，全部為必填欄位。</span>
+                                                                <span class="caption-subject bold uppercase">請確定以下資料正確。</span>
                                                             </div>
                                                         </div>
                                                         <div class="portlet-body form">
@@ -58,25 +58,19 @@
                                                                 <div class="col-md-4">
                                                                     <div class="form-group">
                                                                         <label>團體或個人</label>
-                                                                        <select class="form-control input-lg">
-                                                                            <option>請選擇</option>
-                                                                            <option>團體</option>
-                                                                            <option>個人</option>
-                                                                        </select>
+                                                                        <input type="text" class="form-control input-lg" placeholder="團體" disabled>
                                                                     </div>
                                                                 </div>
                                                                 <div class="col-md-4">
                                                                     <div class="form-group">
                                                                         <label>團體(代表人)名稱</label>
-                                                                        <input type="text" class="form-control input-lg" placeholder="必填">
+                                                                        <input type="text" class="form-control input-lg" placeholder="亞洲大學" disabled>
                                                                     </div>
                                                                 </div>
                                                                 <div class="col-md-4">
                                                                     <div class="form-group">
                                                                         <label>預約參觀人數</label>
-                                                                        <div class="input-group input-group-lg">
-                                                                            <input class="input-lg" id="touchspin_5" type="text" value="" name="demo_vertical"></div>
-                                                                        </div>
+                                                                        <input type="text" class="form-control input-lg" placeholder="20" disabled>
                                                                     </div>
                                                                 </div>
                                                             </div>
@@ -84,31 +78,25 @@
                                                                 <div class="col-md-4">
                                                                     <div class="form-group">
                                                                         <label>姓名</label>
-                                                                        <input type="text" class="form-control input-lg" placeholder="必填">
+                                                                        <input type="text" class="form-control input-lg" placeholder="Vincent Adler" disabled>
                                                                     </div>
                                                                 </div>
                                                                 <div class="col-md-2">
                                                                     <div class="form-group">
-                                                                        <label>性別</label><br>
-                                                                        <input type="checkbox" class="make-switch" checked data-on-color="info" data-off-color="success" data-on-text="男" data-off-text="女" data-size="large">
+                                                                        <label>性別</label>
+                                                                        <input type="text" class="form-control input-lg" placeholder="男" disabled>
                                                                     </div>
                                                                 </div>
                                                                 <div class="col-md-6">
                                                                     <label class="control-label">行動電話</label>
-                                                                    <div>
-                                                                        <input class="form-control input-lg" id="mask_phone" type="text" />
-                                                                    </div>
+                                                                    <input type="text" class="form-control input-lg" placeholder="0912-345-678" disabled>
                                                                 </div>
                                                             </div>
                                                             <div class="row">
                                                                 <div class="col-md-12">
                                                                     <div class="form-group">
                                                                         <label>Email (google 帳戶請使用 gmail.com，yahoo 帳戶請使用 yahoo.com.tw)</label>
-                                                                        <div class="input-group">
-                                                                        <span class="input-group-addon">
-                                                                            <i class="fa fa-envelope"></i>
-                                                                        </span>
-                                                                            <input type="text" class="form-control input-lg" id="mask_email" placeholder="必填"> </div>
+                                                                        <input type="text" class="form-control input-lg" placeholder="asiaUniversity@asia.edu.tw" disabled>
                                                                     </div>
                                                                 </div>
                                                             </div>
@@ -120,12 +108,12 @@
                                                 <br>
                                                 <div class="col-md-6 col-sm-6 col-xs-6">
                                                     <div class="text-center">
-                                                        <a href="{{route('campustour.step2')}}" class="btn blue btn-block btn-lg m-icon-big"><i class="m-icon-big-swapleft m-icon-white"></i> 上一步</a>
+                                                        <a href="{{route('campustour.step3')}}" class="btn blue btn-block btn-lg m-icon-big"><i class="m-icon-big-swapleft m-icon-white"></i> 上一步</a>
                                                     </div>
                                                 </div>
                                                 <div class="col-md-6 col-sm-6 col-xs-6">
                                                     <div class="text-center">
-                                                        <a href="/" id="step1next" class="btn green-jungle btn-block btn-lg m-icon-big">下一步 <i class="m-icon-big-swapright m-icon-white"></i></a>
+                                                        <a href="/" id="step1next" class="btn green-jungle btn-block btn-lg m-icon-big">完成預約 <i class="m-icon-big-swapright m-icon-white"></i></a>
                                                     </div>
                                                 </div>
                                                 <!-- END Portlet PORTLET-->
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,4 @@ Route::get('/', function () {
 Route::get('campustour/step1', 'CampusTourStep1Controller@index')->name('campustour.step1');
 Route::get('campustour/step2', 'CampusTourStep2Controller@index')->name('campustour.step2');
 Route::get('campustour/step3', 'CampusTourStep3Controller@index')->name('campustour.step3');
+Route::get('campustour/step4', 'CampusTourStep4Controller@index')->name('campustour.step4');
